6407355a7a4747e38a7424e3439dc8782074ab28
DESKTOP-6GQ38RC\Tugay <tgy.uner@gmail.com>
2018-02-21 16:11:53 +0300

    Initial commit

---
+++ SignalR_Example/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalR_Example.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.Others.GetMessageOther(name, message);
+
+            Clients.Caller.GetMessageCaller(message);
+
+        }
+    }
+}
